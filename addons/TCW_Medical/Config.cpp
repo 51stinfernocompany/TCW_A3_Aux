85d467d11d351db2a95b78f826c49c536afdf4b0
kpdbos <kpdbos@gmail.com>
2026-05-28 22:57:29 -0400

    Medical Rename plus Icons

---
+++ addons/TCW_Medical/Config.cpp
@@ -0,0 +1,55 @@
+class CfgWeapons {
+    class ACE_elasticBandage;
+    class TCW_bactaSpray : ACE_elasticBandage {
+        displayName = "[TCW] Bacta Spray";
+        picture="x\tcw\addons\TCW_Medical\ui\Bacta_Spray_ca.paa";
+    };
+ 
+    class ACE_surgicalKit;
+    class TCW_stitchKit : ACE_surgicalKit {
+        displayName = "[TCW] Stitch Kit";
+    };
+ 
+    class ACE_epinephrine;
+    class TCW_adrenalStim : ACE_epinephrine {
+        displayName = "[TCW] Adrenal Stim";
+    };
+ 
+    class ACE_morphine;
+    class TCW_combatStim : ACE_morphine {
+        displayName = "[TCW] Combat Stim";
+    };
+ 
+    class ACE_plasmaIV;
+    class TCW_bacta : ACE_plasmaIV {
+        displayName = "[TCW] Bacta(1000ml)";
+        picture="x\tcw\addons\TCW_Medical\ui\ivbag_bacta_1000ml_ca.paa";
+    };
+
+    class ACE_plasmaIV_500;
+    class TCW_bacta_500 : ACE_plasmaIV_500 {
+        displayName = "[TCW] Bacta(500ml)";
+        picture="x\tcw\addons\TCW_Medical\ui\ivbag_bacta_500ml_ca.paa";
+    };
+
+    class ACE_plasmaIV_250;
+    class TCW_bacta_250 : ACE_plasmaIV_250 {
+        displayName = "[TCW] Bacta(250ml)";
+        picture="x\tcw\addons\TCW_Medical\ui\ivbag_bacta_250ml_ca.paa";
+    };
+ 
+    class ACE_tourniquet;
+    class TCW_tourniquet : ACE_tourniquet {
+        displayName = "[TCW] Tourniquet";
+    };
+ 
+    class ACE_splint;
+    class TCW_splint : ACE_splint {
+        displayName = "[TCW] Splint";
+    };
+ 
+    class ACE_AED;
+    class TCW_electroPad : ACE_AED {
+        displayName = "[TCW] Electro-Pad";
+    };
+};
